1d09361411fd2efef9070e7b563d8d15e553a764
Rameş Aliyev <creator@ramesaliyev.com>
2020-02-14 16:44:22 +0300

    :sparkles: add graph method

---
+++ graph.py
@@ -0,0 +1,39 @@
+def fn(x):
+  return (x ** 2) - (6*x) + 5
+
+# 1 and 5
+
+def hasSameSign(a, b):
+  return (a >= 0) == (b >= 0)
+
+def graph(x, dx=1, epsilon=0.1):
+  prevX = x
+  prevY = fn(x)
+
+  while True:
+    x += dx
+    y = fn(x)
+
+    if (y == 0):
+      return x
+
+    if hasSameSign(y, prevY):
+      prevY = y
+      prevX = x
+    elif abs(x - prevX) < epsilon:
+      return (x + prevX) / 2
+    else:
+      return graph(prevX, dx/2, epsilon)
+
+print(fn(1))
+print(fn(5))
+
+print(hasSameSign(0, 0))
+print(hasSameSign(1, 2))
+print(hasSameSign(-1, 1))
+
+print(graph(0))
+print(graph(2))
+
+print(graph(0, epsilon=0.001))
+print(graph(2, epsilon=0.001))
