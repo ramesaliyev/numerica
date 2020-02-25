653a08410316f0d41c2fa52dd168b30be7cf5fb4
Rameş Aliyev <creator@ramesaliyev.com>
2020-02-25 20:54:12 +0300

    :package: 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setup(
   name='numerica',
-  version='0.1.4',
+  version='0.1.5',
   description='Numerical Analysis methods with Python (experimental)',
   long_description=long_description,
   long_description_content_type='text/markdown',
