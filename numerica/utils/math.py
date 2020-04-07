cb39a2d2d2a21cb6eba4dadf870ce0e3e16dac00
Rameş Aliyev <creator@ramesaliyev.com>
2020-04-07 23:53:00 +0300

    :sparkles: add permutation util with naive approach

--- numerica/utils/math.py
+++ numerica/utils/math.py
@@ -1,2 +1,15 @@
 def haveSameSign(a, b):
   return (a >= 0) == (b >= 0)
+
+def permutation(items):
+  if len(items) == 1:
+    return items
+
+  result = []
+  for (i, n) in enumerate(items):
+    rest = items[:i] + items[(i + 1):]
+
+    for m in permutation(rest):
+      result.append([n] + (m if type(m) == list else [m]))
+
+  return result
