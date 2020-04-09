15da9d468237c92d0b102712e3a6a79efcd2ff10
Rameş Aliyev <creator@ramesaliyev.com>
2020-04-09 04:28:22 +0300

    :recycle: use permutation util with memory efficient generators approach

--- numerica/utils/math.py
+++ numerica/utils/math.py
@@ -2,14 +2,9 @@ def haveSameSign(a, b):
   return (a >= 0) == (b >= 0)
 
 def permutation(items):
-  if len(items) == 1:
-    return items
-
-  result = []
-  for (i, n) in enumerate(items):
-    rest = items[:i] + items[(i + 1):]
-
-    for m in permutation(rest):
-      result.append([n] + (m if type(m) == list else [m]))
-
-  return result
+  if len(items) <=1:
+    yield items
+  else:
+    for perm in permutation(items[1:]):
+      for i in range(len(items)):
+        yield perm[:i] + items[0:1] + perm[i:]
